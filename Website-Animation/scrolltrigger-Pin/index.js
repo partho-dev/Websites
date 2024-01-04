7eb1df64f1cfd095f4bfd12fdfd30f38423911d9
MacBook <macbook@MacBooks-MacBook-Pro.local>
2024-01-04 17:06:05 +0530

    gsap

---
+++ Website-Animation/scrolltrigger-Pin/index.js
@@ -0,0 +1,46 @@
+
+gsap.from(".page1 h1", {
+    y:-100,
+    duration:2
+})
+
+gsap.to(".page1 .box1",{
+    x:"400%",
+    y:"120%",
+    rotate: 720,
+    scrollTrigger:{
+        trigger:".page1",
+        scrollers:"body",
+        start:"top 0",
+        end:"top -100%",
+        scrub:2,
+        pin:true
+    }
+})
+
+gsap.to(".page2 img", {
+    width:"100%",
+    opacity:1,
+    scrollTrigger:{
+        trigger:".page2",
+        scroller:"body",
+        // markers:true,
+        start:"top 0",
+        end:"top -100%",
+        scrub: 2,
+        pin:true
+    }
+})
+
+gsap.to(".page3 h1", {
+    transform: "translateX(-125%)", /* This is needed to enable horizontal scrolling*/
+    scrollTrigger:{
+        trigger:".page3",
+        scroller:"body",
+        // markers:true,
+        start:"top 0",
+        end:"top -100%",
+        scrub:2,
+        pin:true
+    }
+})
